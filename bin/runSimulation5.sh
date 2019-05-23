58fa5eba071516b32f00228c3a08d0f58343073e
Adrian K <adriankiratidis@gmail.com>
2019-05-22 20:49:20 -0700

    reconnected to git after OS crash

--- bin/runSimulation5.sh
+++ bin/runSimulation5.sh
@@ -1,7 +1,7 @@
 #### Shell script to run runSingleSphere.csh
 #!/bin/bash
 
-declare -a epsilons=(35.51)
+declare -a epsilons=(53.27)
 
 for i in "${epsilons[@]}"
 do
@@ -11,17 +11,18 @@ test_file_stub="testing5-a2"
 
 #ionic_liquid_name="NeutralDimers"
 #ionic_liquid_name="SingleNeutralSpheres"
-ionic_liquid_name="C4MIM_BF4-"
+#ionic_liquid_name="C4MIM_BF4-"
 #ionic_liquid_name="PositiveMinusSpheres"
 #ionic_liquid_name="PositiveNeutralMinusSpheres"
 #ionic_liquid_name="PositiveNeutralDimerMinusSpheres"
 #ionic_liquid_name="C6MIM+_TFSI-_model1"
 #ionic_liquid_name="C4MIM+_TFSI-_model1"
 #ionic_liquid_name="PositiveNeutralDoubleDimerMinusDimer"
+ionic_liquid_name="Heptamers"
 chi_parameter=0.71
 Epsilon_r=14.0
 Epsilon_LJ_particle_particle=35.51 # ( = epsilon_LJ * k_{B})
-epsilon_eighth_power_const=2.3810
+epsilon_eighth_power_const=60
 #Epsilon_LJ_particle_wall=100
 #mica_density=0.00505293988 #Mica density in particles/angstrom^3
 mica_density=0.10611173748 #Mica density in particles/angstrom^3
@@ -31,6 +32,7 @@ mica_density=0.10611173748 #Mica density in particles/angstrom^3
 surface_charge_density_left_wall=-0.003125
 surface_charge_density_right_wall=-0.003125
 hs_diameter=2.4
+hs_fraction=1.0
 a_term_index=2
 #bulk_density=0.04475			# ( =n_{b} * [hs_diameter**3] )
 #bulk_density=0.01467			# ( =n_{b} * [hs_diameter**3] )
@@ -40,17 +42,17 @@ alpha_mixing_for_update=0.05
 slope_for_initial_guess=0.000
 #slope_for_initial_guess=0.0000
 n_charge_iterations=1
-positive_bead_charge=0.2
-negative_bead_charge=-0.2
+positive_bead_charge=1.0
+negative_bead_charge=-1.0
 Donan_potential_intial_guess=0.0
 string_length=1.2			#
-n_points_per_hs_diameter=50			#number of discretised points
-max_iteration_limit=25000
+n_points_per_hs_diameter=40			#number of discretised points
+max_iteration_limit=5000
 iterative_tolerance=0.000000000000001
 
 starting_plate_separation=2
-number_of_separations=400  #The number of plate separations we'll calculate.
-number_of_plate_separations_in_hs_diameter=50
+number_of_separations=320  #The number of plate separations we'll calculate.
+number_of_plate_separations_in_hs_diameter=40
 
 params_file=$test_file_stub.params
 
@@ -103,7 +105,7 @@ done
 $test_file_stub
 EOF
 
-write_dir="./run_results/compare_epsilonLJ/CentreCentrePotential/minuswalls/${Epsilon_LJ_particle_particle}-${Epsilon_LJ_particle_wall}_${ionic_liquid_name}-${bulk_density}-${epsilon_eighth_power_const}-${surface_charge_density_left_wall}"
+write_dir="./run_results/compare_epsilonLJ/CentreCentrePotential/minuswalls/${Epsilon_LJ_particle_particle}-${Epsilon_LJ_particle_wall}_${ionic_liquid_name}-${bulk_density}-${epsilon_eighth_power_const}-${surface_charge_density_left_wall}-hs${hs_fraction}CHARGE"
 mkdir -p ${write_dir}
 mv ${test_file_stub}* ${write_dir}
 
